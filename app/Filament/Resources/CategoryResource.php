aef9ae97d35a86b93ce967cad36820d4787f903f
Polat Kargılı <54178145+aliora@users.noreply.github.com>
2025-09-03 11:50:19 +0300

    Refactor code structure for improved readability and maintainability

--- app/Filament/Resources/CategoryResource.php
+++ app/Filament/Resources/CategoryResource.php
@@ -18,6 +18,7 @@ class CategoryResource extends Resource
     protected static ?string $model = Category::class;
 
     protected static ?string $navigationIcon = 'heroicon-o-rectangle-stack';
+    protected static ?string $navigationGroup = 'Proje Yönetimi';
     protected static ?string $pluralModelLabel = 'Projeler';
     protected static ?string $modelLabel = 'Proje';
 
